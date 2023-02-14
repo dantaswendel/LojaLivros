9baf7a2403800e9732cabcf4d37d2222e6dc04a7
Wendel Dantas Lima <93411167+dantaswendel@users.noreply.github.com>
2023-02-14 15:51:51 -0300

    Mapeando entidades

--- src/main/java/br/com/wendel/lojaLivros/model/Cliente.java
+++ src/main/java/br/com/wendel/lojaLivros/model/Cliente.java
@@ -1,13 +1,25 @@
 package br.com.wendel.lojaLivros.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cliente")
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+    @Column(name = "nome", length = 100)
     private String nome;
+    @Column(name = "telefone", length = 11)
     private String telefone;
+    @Column(name = "email", length = 100)
     private String email;
 
+public Cliente(){
 
+}
 
     public Integer getId() {
         return id;
--- src/main/java/br/com/wendel/lojaLivros/repository/ClientesRepository.java
+++ src/main/java/br/com/wendel/lojaLivros/repository/ClientesRepository.java
@@ -2,20 +2,56 @@ package br.com.wendel.lojaLivros.repository;
 
 
 import br.com.wendel.lojaLivros.model.Cliente;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import org.hibernate.boot.model.relational.SqlStringGenerationContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class ClientesRepository {
 
-    private static String INSERT = "insert into cliente (nome) values (?)";
     @Autowired
-    private static JdbcTemplate jdbcTemplate;
-
+    private static EntityManager entityManager;
+    @Transactional
     public static Cliente salvar(Cliente cliente){
-        jdbcTemplate.update(INSERT, new Object []{cliente.getNome()});
+      entityManager.persist(cliente);
         return cliente;
     }
+    @Transactional
+    public static Cliente atualizar (Cliente cliente){
+        entityManager.merge(cliente);
+        return cliente;
+    }
+    @Transactional
+    public void  deletar (Cliente cliente){
+
+        entityManager.remove(cliente);
+    }
+    @Transactional
+    public void  deletarId (Integer id){
+       Cliente cliente  = entityManager.find(Cliente.class, id);
+       deletar(cliente);
+    }
+
+    @Transactional
+    public List<Cliente> obterTodos (String nome){
+    return entityManager
+            .createQuery(SqlStringGenerationContext: "from Cliente", Cliente.class)
+            .getResultList();
+
+
+    }
+
+
 
 }
+
